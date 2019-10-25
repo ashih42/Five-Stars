89698eb16ebefdd77100079e60e689a646024021
Andy Shih <a.shih@coldwhere.com>
2019-10-25 13:59:49 -0500

    Initial commit

---
+++ src/components/StarRating.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Star from './Star';
+
+class StarRating extends Component {
+  // Initialize a 'rating' state
+  state = {
+    rating: 0
+  }
+
+  // Write a function that returns 5 Star components
+  render_stars() {
+    const maxRating = 5;
+    let stars = [];
+
+    for (let i = 1; i <= maxRating; i++) {
+      stars.push(
+        <Star
+          key={i}
+          isSelected={i <= this.state.rating}
+          setRating={() => this.handleSetRating(i)}
+        />
+      );
+    }
+    return stars;
+  }
+
+  // Write an event handler that updates the rating state.
+  // Pass the function to a Star component via props
+  handleSetRating = (rating) => {
+    // if the star of the current rating is clicked, reset rating to 0
+    if (rating == this.state.rating) {
+      this.setState({ rating: 0 });
+    } else {
+      this.setState({ rating });
+    }
+  }
+
+  render() {
+    return (
+      <ul className="course--stars">
+        { this.render_stars() }
+      </ul>
+    );
+  }
+}
+
+export default StarRating;
